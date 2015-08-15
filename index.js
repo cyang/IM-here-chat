298be0bacc90b17baf8367fdda2e84b7d30700f4
Christopher Yang <cyang001@citymail.cuny.edu>
2015-08-15 10:00:34 -0400

    Input messages in chat

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // Use express framework
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+//	console.log("A user has connected.")
+//	socket.on('disconnect', function(){
+//    	console.log('A user disconnected.');
+//  	});
+	  
+	socket.on('chat message', function(msg){
+    	io.emit('chat message', msg);
+  	});
+  
 });
 
 http.listen(3000, function(){
